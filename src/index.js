28f85f4079223b90ef51151e6b8014378b529da7
MusaddekAli <musaddekali.dev@gmail.com>
2023-11-18 19:42:49 +0600

    app default export added

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-import userRouters from './routes/users.js'
-import githubRouters from './routes/github.js'
+import userRouters from "./routes/users.js";
+import githubRouters from "./routes/github.js";
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 const port = process.env.PORT;
 
-app.use('/api', userRouters);
-app.use('/api', githubRouters)
+app.use("/api", userRouters);
+app.use("/api", githubRouters);
 
-app.get('/api', (req, res) => {
-    res.send('You are in the empty ocan...💖')
-})
-
+app.get("/api", (req, res) => {
+  res.send("You are in the empty ocan...💖");
+});
 
 app.listen(port, () => {
-    console.log(`App is running on http://localhost:${port}`)
-})
+  console.log(`App is running on http://localhost:${port}`);
+});
+
+export default app;
